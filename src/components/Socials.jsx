bb2f0a5b340fd05316978300a8a4b19546bf0184
neducod <nedunony16@gmail.com>
2026-07-17 05:19:31 +0100

    nav section done

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -9,20 +9,39 @@ import { CgMenuRight } from "react-icons/cg";
 import { IoLogoTiktok } from "react-icons/io5";
 import { FaWhatsapp } from "react-icons/fa";
 
+
+
+const FontStyle = () => (
+    <style>{`
+      @import url('https://fonts.googleapis.com/css2?family=Gaegu:wght@400;700&display=swap');
+      .font-handdrawn {
+        font-family: 'Gaegu', cursive, sans-serif;
+      }
+      @keyframes float {
+        0%, 100% { transform: translateY(0px); }
+        50% { transform: translateY(-6px); }
+      }
+      .animate-float {
+        animation: float 4s ease-in-out infinite;
+      }
+    `}</style>
+);
+
+
 function Home() {
     return (
         <>
-            <nav>
+            <nav className='bg-black text-white flex justify-around md:p-10'>
                 <div>
                     <h1>Joella's Art</h1>
                 </div>
                 <div>
-                    <div>
+                    <div className='md:hidden'>
                         <CgMenuRight />
                     </div>
-                    <div>
-                        {/* on desktop */}
-                        <ul>
+                    <div className='hidden md:block'>
+                        <ul className='flex gap-8'>
+                            <FontStyle/>
                             <li>Comics</li>
                             <li>Merchandise</li>
                             <li>Shop</li>
@@ -33,20 +52,19 @@ function Home() {
                 </div>
             </nav>
 
-            <section>
-                {/* THIS WOULD BE MY HERO SECTION */}
+            <section className='bg-black text-white'>
                 <h2>LET'S CONNECT</h2>
-                <img src={SocialHero} alt="" />
+                <img src={SocialHero} alt="" className='w-40' />
                 <p>Joella's Art</p>
             </section>
 
-            <section>
+            <section className='bg-black'>
                 <FaInstagram />
                 <IoLogoTiktok />
                 <FaWhatsapp />
             </section>
 
-            <section className='flex'>
+            <section className='flex bg-black text-white'>
                 <div>
                     <a href="https://www.instagram.com/joellarts._/">
                         <img src={SocialsTwo} alt="" />
