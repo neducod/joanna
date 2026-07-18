8ed80e3d78e21daf8b80e454e82ca6fe6f75a020
neducod <nedunony16@gmail.com>
2026-07-18 03:40:15 +0100

    dynamic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <>
+            <Footer className="bg-black flex justify-end">
+                <p>@2026 <a href="#">Josephine Jerome</a></p>
+
+            </Footer>
+        </>
+    )
+}
+
+export default Footer;
