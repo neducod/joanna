24dd957dbccbf65723749c4cf03038ff220e7d2b
neducod <nedunony16@gmail.com>
2026-07-18 02:33:45 +0100

    support me section styling

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CgMenuRight } from "react-icons/cg";
 // import './App.css'
 
 
@@ -6,9 +7,30 @@ import React from 'react';
 function Home() {
     return (
         <>
-            <section>
-                <h2>Hello, you could be part of bringing my dreams to reality</h2>
-                <p>I am a digital artist and i ahev limited tools and resources to work, if you could be part of my art journey into turning my dreams to realtiy where i create beautiful digital art works</p>
+            <nav className='bg-black text-white flex justify-around md:p-10 md:gap-20'>
+                <div>
+                    <h1>Joella's Art</h1>
+                </div>
+                <div>
+                    <div className='md:hidden'>
+                        <CgMenuRight />
+                    </div>
+                    <div className='hidden md:block'>
+                        <ul className='flex gap-10'>
+                            {/* <FontStyle/> */}
+                            <li>About</li>
+                            <li>Art Journey</li>
+                            <li>Support me</li>
+                            <li>Work with me</li>
+                            <li>Social media</li>
+                            <li>Gallery</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <section className='bg-[#cf9fff] flex justify-center items-center p-20 flex-col gap-6'>
+                <h2 className='text-3xl w-3xl'>Hello, you could be part of bringing my dreams to reality</h2>
+                <p className='w-5xl text-center'>I am a digital artist and i ahev limited tools and resources to work, if you could be part of my art journey into turning my dreams to realtiy where i create beautiful digital art works</p>
             </section>
             <section>
                 <h2>How you can help</h2>
@@ -25,6 +47,15 @@ function Home() {
                     <div>
                         <p>Tablet</p>
                     </div>
+                    <div>
+                        <p>Hard cover</p>
+                    </div>
+                    <div>
+                        <p>Pencils</p>
+                    </div>
+                    <div>
+                        <p>Headsets</p>
+                    </div>
                 </div>
             </section>
             <section>
@@ -36,7 +67,6 @@ function Home() {
                     <p>How do you plan on getting it</p>
                 </div>
             </section>
-        <h1>hi</h1>
 
         </>
     )
